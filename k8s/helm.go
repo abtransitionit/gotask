8fa87414e9ae32fd6302b83cf3d8431e1fe761f8
abelgacem <amarbelgacem@hotmail.fr>
2025-10-06 08:32:15 +0200

    update

---
+++ k8s/helm.go
@@ -0,0 +1,15 @@
+package k8s
+
+import (
+	"context"
+
+	"github.com/abtransitionit/gocore/logx"
+)
+
+func installSingleRepoOnSingleVm(ctx context.Context, logger logx.Logger) {
+	logx.Info("install Single Repo On Single Vm")
+}
+
+func installListRepoOnSingleVm(ctx context.Context, logger logx.Logger) {
+	logx.Info("install List Repo On Single Vm")
+}
