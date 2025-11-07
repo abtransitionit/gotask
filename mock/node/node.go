372f2ddcd356fbcb2a903833ba76d7e2fa50dd94
abelgacem <amarbelgacem@hotmail.fr>
2025-11-07 14:34:22 +0100

    update

---
+++ mock/node/node.go
@@ -0,0 +1,40 @@
+package node
+
+import (
+	"fmt"
+
+	"github.com/abtransitionit/gocore/logx"
+	lnode "github.com/abtransitionit/golinux/mock/node"
+)
+
+// Description: check if a set of nodes are SSH configured.
+func CheckSshConf(nodes []string, logger logx.Logger) (bool, error) {
+	results := make(map[string]bool)
+
+	for _, node := range nodes {
+		ok, err := lnode.IsSshConfigured(node, logger)
+		if err != nil {
+			return false, fmt.Errorf("checking SSH config for node %q: %w", node, err)
+		}
+		results[node] = ok
+		logger.Infof("Node %q: SSH configured = %v", node, ok)
+	}
+	// success
+	return true, nil
+}
+
+// Description: check if a set of nodes are SSH reachable.
+func CheckSshAccess(nodes []string, logger logx.Logger) (bool, error) {
+	results := make(map[string]bool)
+
+	for _, node := range nodes {
+		ok, err := lnode.IsSshReachable(node, logger)
+		if err != nil {
+			return false, fmt.Errorf("node %q: error checking SSH reachability: %w", node, err)
+		}
+		results[node] = ok
+		logger.Infof("Node %q: SSH reachable = %v", node, ok)
+	}
+	// success
+	return true, nil
+}
