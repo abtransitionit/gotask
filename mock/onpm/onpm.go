bca53d07d5115578794e7340f938b1ed3d2f3d22
abelgacem <amarbelgacem@hotmail.fr>
2025-11-19 15:38:21 +0100

    update

--- mock/onpm/onpm.go
+++ mock/onpm/onpm.go
@@ -1,8 +1,6 @@
 package onpm
 
 import (
-	"fmt"
-
 	"github.com/abtransitionit/gocore/logx"
 	lonpm "github.com/abtransitionit/golinux/mock/onpm"
 )
@@ -15,33 +13,12 @@ import (
 // - upgrade mean: set the OS native repositories and packages to the latest version
 func UpgradeOs(targetName string, paramList [][]any, logger logx.Logger) (bool, error) {
 
-	// 1 - extract parameters
-	nodeList := []string{}
-	for _, v := range paramList[0] {
-		nodeList = append(nodeList, fmt.Sprint(v)) // converts any -> string
-	}
+	// play CLI
+	_, err := lonpm.UpgradeOs(targetName, logger)
 
-	// define var
-	// results := make(map[string]bool) // collector
-	var failedNodes []string // slice of node that are not SSH configured
-
-	// loop over each node
-	for _, node := range nodeList {
-
-		// play CLI - check if SSH is configured for the couple target/node
-		_, err := lonpm.UpgradeOs(logger)
-
-		// handle system error
-		if err != nil {
-			logger.Warnf("target: %s > Node %s: > system error > checking SSH config: %v", targetName, node, err)
-			continue
-		}
-
-	}
-
-	// If any node failed, return a single error message
-	if len(failedNodes) > 0 {
-		return false, fmt.Errorf("target: %s > Node(s) that are not SSH configured: %v", targetName, failedNodes)
+	// handle system error
+	if err != nil {
+		logger.Warnf("target: %s > system error > checking SSH config: %v", targetName, err)
 	}
 
 	// handle success
