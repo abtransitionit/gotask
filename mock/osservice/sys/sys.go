f44f856f9b8ffef1252280a5ad023c9fce37e676
abelgacem <amarbelgacem@hotmail.fr>
2025-12-25 11:36:12 +0100

    update

--- mock/osservice/sys/sys.go
+++ mock/osservice/sys/sys.go
@@ -29,7 +29,7 @@ func Start(phaseName, hostName string, paramList [][]any, logger logx.Logger) (b
 		// get instance
 		osService := osservice.GetService(item.Name)
 		// operate
-		if _, err := osService.Start(logger); err != nil {
+		if _, err := osService.Start(hostName, logger); err != nil {
 			// send error if any into the chanel
 			errChItem <- fmt.Errorf("enabling os service %s: %w", item.Name, err)
 		}
@@ -75,7 +75,7 @@ func Enable(phaseName, hostName string, paramList [][]any, logger logx.Logger) (
 		// get instance
 		osService := osservice.GetService(item.Name)
 		// operate
-		if _, err := osService.Enable(logger); err != nil {
+		if _, err := osService.Enable(hostName, logger); err != nil {
 			// send error if any into the chanel
 			errChItem <- fmt.Errorf("enabling os service %s: %w", item.Name, err)
 		}
@@ -94,6 +94,52 @@ func Enable(phaseName, hostName string, paramList [][]any, logger logx.Logger) (
 	errCombined := errors.Join(errList...)
 	if nbGroutineFailed > 0 {
 		logger.Errorf("❌ %s > nb service enabling that failed: %d", hostName, nbGroutineFailed)
+		return false, errCombined
+	}
+
+	// handle success
+	return true, nil
+}
+
+func Install(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
+	// 1 - get parameters
+	// 11 - list of service
+	if len(paramList) < 1 || len(paramList[0]) == 0 {
+		return false, fmt.Errorf("%s > serviceList not provided in paramList", hostName)
+	}
+	serviceSlice, err := filex.GetVarStructFromYaml[osservice.ServiceSlice](paramList[0])
+	if err != nil {
+		logger.Errorf("%v", err)
+	}
+
+	// 2 - manage error reporting
+	nbItem := len(serviceSlice)
+	errChItem := make(chan error, nbItem) // define a channel to collect errors
+
+	// 3 - loop over item
+	for _, item := range serviceSlice {
+		// get instance
+		osService := osservice.GetService(item.Name)
+		// operate
+		if _, err := osService.Install(hostName, logger); err != nil {
+			// send error if any into the chanel
+			errChItem <- fmt.Errorf("installing os service %s: %w", item.Name, err)
+		}
+	} // loop
+
+	// 4 - manage error
+	close(errChItem) // close the channel - signal that no more error will be sent
+	// 41 - collect errors
+	var errList []error
+	for e := range errChItem {
+		errList = append(errList, e)
+	}
+
+	// 42 - handle errors
+	nbGroutineFailed := len(errList)
+	errCombined := errors.Join(errList...)
+	if nbGroutineFailed > 0 {
+		logger.Errorf("❌ %s > nb service installing that failed: %d", hostName, nbGroutineFailed)
 		return false, errCombined
 	}
 
