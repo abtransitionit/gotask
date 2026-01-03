ccd8c0cb9723cf84570136379f39bf97c790ca18
abelgacem <amarbelgacem@hotmail.fr>
2026-01-03 18:09:20 +0100

    update

--- mock/k8s/helm.go
+++ mock/k8s/helm.go
@@ -5,24 +5,45 @@ import (
 )
 
 // Description: Add helm repo to a Helm client
+//
+// Notes:
+//
+// - it just adds the repo to the Helm client, it does not install any chart
 func AddRepoHelm(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
 	// log
-	logger.Info("AddRepoHelm called")
+	logger.Info("AddRepoHelm : adding repo: TODO")
 	// 1 - get parameters
 	// 11 - List helm repo
-	logger.Info("AddRepoHelm : get list repo")
 
 	// handle success
 	return true, nil
 }
 
-// Description: Add helm charts into to a K8s cluster from a Helm client
+// Description: Add helm charts into a K8s cluster from a Helm client
+//
+// Notes:
+//
+// - the helm repo of the chart must be presents in the Helm client
 func AddChartHelm(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
 	// log
-	logger.Info("AddChartHelm called")
+	logger.Info("AddChartHelm : adding chart: TODO")
 	// 1 - get parameters
 	// 11 - List helm chart
-	logger.Info("AddChartHelm : get list chart")
+
+	// handle success
+	return true, nil
+}
+
+// Description: Add helm releases into a K8s cluster from a Helm client
+//
+// Notes:
+//
+// - the helm repo of the chart must be presents in the Helm client
+func AddReleaseHelm(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
+	// log
+	logger.Info("AddReleaseHelm : adding release: TODO")
+	// 1 - get parameters
+	// 11 - List helm release
 
 	// handle success
 	return true, nil
