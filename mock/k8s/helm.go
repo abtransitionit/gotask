8bc16b8229f840d9674ffd8e1ba9e07b2e3428a6
abelgacem <amarbelgacem@hotmail.fr>
2026-01-11 14:53:05 +0100

    update

--- mock/k8s/helm.go
+++ mock/k8s/helm.go
@@ -40,7 +40,7 @@ func AddRepoHelm(phaseName, hostName string, paramList [][]any, logger logx.Logg
 		// 32 - operate
 		if err := i.Add(hostName, helmClientNodeName, logger); err != nil {
 			// send error if any into the chanel
-			errChItem <- fmt.Errorf("installing Helm repo %s: %w", item.Name, err)
+			errChItem <- fmt.Errorf("adding Helm repo %s: %w", item.Name, err)
 		}
 	} // loop
 
@@ -75,6 +75,21 @@ func AddChartHelm(phaseName, hostName string, paramList [][]any, logger logx.Log
 	return true, nil
 }
 
+// func InstallReleaseHelm(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
+// 	// 1 - get parameters
+// 	// 10 - check
+// 	if len(paramList) < 1 || len(paramList[0]) == 0 || len(paramList[1]) == 0 {
+// 		return false, fmt.Errorf("%s:helm > list repos or helm client not properly provided in paramList", hostName)
+// 	}
+// 	// log
+// 	logger.Info("AddReleaseHelm : adding release: TODO")
+// 	// 1 - get parameters
+// 	// 11 - List helm release
+
+// 	// handle success
+// 	return true, nil
+// }
+
 // Description: Add a helm release from a Helm Chart into a K8s cluster's namespaces
 //
 // Notes:
@@ -84,13 +99,49 @@ func InstallReleaseHelm(phaseName, hostName string, paramList [][]any, logger lo
 	// 1 - get parameters
 	// 10 - check
 	if len(paramList) < 1 || len(paramList[0]) == 0 || len(paramList[1]) == 0 {
-		return false, fmt.Errorf("%s:helm > list repos or helm client not properly provided in paramList", hostName)
+		return false, fmt.Errorf("%s:helm > list releases or helm client not properly provided in paramList", hostName)
 	}
-	// log
-	logger.Info("AddReleaseHelm : adding release: TODO")
-	// 1 - get parameters
-	// 11 - List helm release
+	// 11 - name of helm client node
+	helmClientNodeName := fmt.Sprint(paramList[1][0])
+
+	// 12 - List of helm release
+	slice, err := filex.GetVarStructFromYaml[helm.ReleaseSlice](paramList[0])
+	if err != nil {
+		return false, fmt.Errorf("%s:helm > getting list from paramList: %w", hostName, err)
+	}
+
+	// 2 - manage error reporting
+	nbItem := len(slice)
+	errChItem := make(chan error, nbItem) // define a channel to collect errors
+
+	// 3 - loop over item
+	for _, item := range slice {
+		// 31 - get instance
+		i := helm.GetRelease(item.Name, item.CQName, item.Version, item.Namespace)
+		// 32 - operate
+		if err := i.Install(hostName, helmClientNodeName, logger); err != nil {
+			// send error if any into the chanel
+			errChItem <- fmt.Errorf("installing Helm release %s: %w", item.Name, err)
+		}
+	} // loop
+
+	// 4 - manage error
+	close(errChItem) // close the channel - signal that no more error will be sent
+	// 41 - collect errors
+	var errList []error
+	for e := range errChItem {
+		errList = append(errList, e)
+	}
+
+	// 42 - handle errors
+	nbGroutineFailed := len(errList)
+	errCombined := errors.Join(errList...)
+	if nbGroutineFailed > 0 {
+		logger.Errorf("❌ %s > nb item that failed: %d", hostName, nbGroutineFailed)
+		return false, errCombined
+	}
 
 	// handle success
+	// logger.Infof("%s:%s > added repo %v on helm client", hostName, helmClientNodeName, slice)
 	return true, nil
 }
