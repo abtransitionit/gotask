c01db90644fd2a746b55d558ec97b7e2e2d22d58
abelgacem <amarbelgacem@hotmail.fr>
2026-01-13 17:45:18 +0100

    update

--- mock/k8s/ingress.go
+++ mock/k8s/ingress.go
@@ -2,28 +2,32 @@ package k8s
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/abtransitionit/gocore/logx"
+	"github.com/abtransitionit/gocore/mock/filex"
+	helm2 "github.com/abtransitionit/golinux/mock/k8scli/helm"
 )
 
-func InstallIngressCilium(phaseName, hostName string, paramList [][]any, logger logx.Logger) (bool, error) {
+func InstallIngressCilium(phaseName, helmHost string, paramList [][]any, logger logx.Logger) (bool, error) {
 	// 1 - get parameters
 	// 10 - check
 	if len(paramList) < 1 || len(paramList[0]) == 0 {
-		return false, fmt.Errorf("%s > release name not provided in paramList", hostName)
+		return false, fmt.Errorf("local:%s > release name or namespace not properly provided in paramList", helmHost)
+	}
+	// 11 - get release to upgrade
+	release, err := filex.GetVarStructFromYaml[helm2.Release](paramList[0][0])
+	if err != nil {
+		return false, fmt.Errorf("local:%s > getting list from paramList: %w", helmHost, err)
+	}
+	// 2 - get Instance
+	i := helm2.GetRelease(release.Name, strings.TrimSpace(release.Chart.QName), "", release.Namespace, nil)
+
+	// 3 - operate
+	if err := i.InstallIngressCilium("local", helmHost, logger); err != nil {
+		return false, fmt.Errorf("local:%s > installing ingress cilium to existing release > %w", helmHost, err)
 	}
 
-	// 11 - get name of the release
-	helmReleaseName := fmt.Sprint(paramList[0][0])
-	// 11 - get Instance
-	// i := k8s.GetNode(hostName)
-
-	// // 2 - operate
-	// if _, err := i.Reset(logger); err != nil {
-	// 	return false, fmt.Errorf("%s > Resetting Node > %v", hostName, err)
-	// }
-
 	// handle success
-	logger.Debugf("%s:%s > installed ingress cilium", hostName, helmReleaseName)
 	return true, nil
 }
